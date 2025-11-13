68fac28600f9e71e60d42804a7ae0f588b922096
Dev-Laolu <olayinkahopewell@gmail.com>
2025-11-13 18:56:51 +0100

    second-7 commit

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,27 @@
+// task_3/js/main.ts
+
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
+
+// Create row object
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva"
+};
+
+// Insert the row
+const newRowID: RowID = CRUD.insertRow(row);
+
+// Create updated row with age
+const updatedRow: RowElement = {
+  ...row,
+  age: 23
+};
+
+// Update the row
+CRUD.updateRow(newRowID, updatedRow);
+
+// Delete the row
+CRUD.deleteRow(newRowID);
