a195e7278a91d43add32023d609a6e51f22e895f
Dev-Laolu <olayinkahopewell@gmail.com>
2025-11-13 19:00:22 +0100

    second-7 commit

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -2,6 +2,7 @@
 
 import { RowID, RowElement } from './interface';
 
+// Use RowElement for row parameter and RowID for return type
 export declare function insertRow(row: RowElement): RowID;
 export declare function deleteRow(rowId: RowID): void;
 export declare function updateRow(rowId: RowID, row: RowElement): RowID;
